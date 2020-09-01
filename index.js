c14e69fd33f8c482f6d09444fb1f9a10e49d952a
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-09-01 21:11:37 +0200

    exports treeshakeable(?) web components

---
+++ index.js
@@ -0,0 +1,4 @@
+export const CreatedByMe = import('./components/createdByMe.js')
+export const Twitter = import('./components/twitter.js')
+export const GitHub = import('./components/github.js')
+export const HomePage = import('./components/homepage.js')
